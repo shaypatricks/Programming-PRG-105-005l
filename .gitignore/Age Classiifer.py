09d6a0c24ddb7e2d7396bfcc12ed0a8057064a73
shaypatricks <31227039+shaypatricks@users.noreply.github.com>
2017-09-17 12:10:19 -0700

    Add files via upload

---
+++ .gitignore/Age Classiifer.py
@@ -0,0 +1,13 @@
+# Its time to enter a age classifier
+
+user_Age =int(input("What is your age. "))
+
+if user_Age <= 1:
+    print("your a infant...enjoy your life!")
+elif user_Age < 13:
+    print(" Your are a child. Have fun like one.")
+elif 13 < user_Age < 20:
+    print(" Your a Teenager! Stop being Moody.")
+elif user_Age > 20:
+    print("Your an adult... Now act like one.")
+# So the Program is done and is error free plus i added my own twist to it
